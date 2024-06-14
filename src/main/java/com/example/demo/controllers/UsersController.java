7b2e7bc2da6bc5437247e15c40bd007f1ad6b3fc
eMongoose <eMongoose@cs.sfu.ca>
2024-06-13 23:04:20 -0700

    added several files

--- src/main/java/com/example/demo/controllers/UsersController.java
+++ src/main/java/com/example/demo/controllers/UsersController.java
@@ -1,24 +1,66 @@
 package com.example.demo.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.example.demo.models.Users;
+import com.example.demo.models.User;
+import com.example.demo.models.UserRepository;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class UsersController {
+    @Autowired
+    private UserRepository userRepo;
+
+    // view rectangles in data base
     @GetMapping("/users/view") // define an endpoint
     public String getAllUsers(Model model) {
         System.out.println("Get all users");
-        List<Users> users = new ArrayList<>();
-        users.add(new Users("emily", 50, 50, "red"));
+        List<User> users = userRepo.findAll();
+        //users.add(new User("emily", 50, 50, "red"));
         // end of database call
 
         model.addAttribute("us", users);
         return "users/showAll";
+    }
+    // ================================================================================================
+
+    // add rectangle to database
+    @PostMapping("/users/add")
+    // extract parameters from request
+    public String addUser(@RequestParam Map<String, String> newuser, HttpServletResponse response) {
+        System.out.println("ADD user"); // console log, user has been added.
+
+        // parsing data
+        String name = newuser.get("name");
+        int width = Integer.parseInt(newuser.get("width")); // Correctly parse int
+        int height = Integer.parseInt(newuser.get("height")); // Correctly parse int
+        String color = newuser.get("color"); // Directly use the String
+
+        // create and save the user object
+        userRepo.save(new User(name, width, height, color));
+        response.setStatus(201); // created a new object
+        return "redirect:/users/view";
+    }
+
+    // ================================================================================================
+    // remove rectangle from database
+    @PostMapping("/users/delete/{id}")
+    public String deleteUser(@PathVariable("id") int id, RedirectAttributes redirectAttributes) {
+        System.out.println("DELETE user"); // console log, user has been deleted.
+
+        // delete rectangle
+        userRepo.deleteById(id);
+        return "redirect:/users/view";
     }
 }
--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -1,17 +1,27 @@
 package com.example.demo.models;
 
-public class Users {
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int uid;
+    
     private String name;
     private int width;
     private int height;
     private String color;
+    
+    private int size;
 
     // constructor does not take in parameters
-    public Users() { 
+    public User() { 
     }
 
     // constructor that takes in parameters
-    public Users(String name, int width, int height, String color) {
+    public User(String name, int width, int height, String color) {
         this.name = name;
         this.width = width;
         this.height = height;
@@ -49,6 +59,22 @@ public class Users {
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
     }
 
 }
